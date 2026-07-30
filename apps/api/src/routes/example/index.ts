f56a8668379e25640b92e20342bb165b78fb9ba0
alexxuss1996 <alekseymelnych132@gmail.com>
2026-07-30 18:49:30 +0300

    init user commit

---
+++ apps/api/src/routes/example/index.ts
@@ -0,0 +1,9 @@
+import { type FastifyPluginAsync } from "fastify";
+
+const example: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
+  fastify.get("/", async function (request, reply) {
+    return { foo: "bar" };
+  });
+};
+
+export default example;
