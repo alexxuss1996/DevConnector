8cad8b3a2495b4d14847cf450ad89967b34b123d
alexxuss1996 <alekseymelnych132@gmail.com>
2026-09-18 12:37:42 +0300

    add some deps  for frontend and rename repo symlink

--- packages/ui/eslint.config.mjs
+++ packages/ui/eslint.config.mjs
@@ -1,4 +1,4 @@
-import { config } from "@repo/eslint-config/react-internal";
+import { config } from "@dev-conn/eslint-config/react-internal";
 
 /** @type {import("eslint").Linter.Config} */
 export default config;
